c2ea5a89673fbcfbd53773ff92830bffa09771cc
Patrick Toner <pat.toner2@gmail.com>
2019-01-19 09:54:16 -0500

    Convert exceptions into classes

--- src/exceptions/ipfs-exception.js
+++ src/exceptions/ipfs-exception.js
@@ -1,14 +1,16 @@
-function IpfsException(message) {
-    this.message = message;
-    // Use V8's native method if available, otherwise fallback
-    if ("captureStackTrace" in Error)
-        Error.captureStackTrace(this, IpfsException);
-    else
-        this.stack = (new Error()).stack;
-}
+class IpfsException extends Error {
 
-IpfsException.prototype = Object.create(Error.prototype);
-IpfsException.prototype.name = "IpfsException";
-IpfsException.prototype.constructor = IpfsException;
+    constructor(message) {
+        super(message)
+        this.name = "IpfsException"
+
+        // Use V8's native method if available, otherwise fallback
+        if ("captureStackTrace" in Error)
+            Error.captureStackTrace(this, IpfsException);
+        else
+            this.stack = (new Error()).stack;
+    }
+
+}
 
 module.exports = IpfsException;
--- src/exceptions/validation-exception.js
+++ src/exceptions/validation-exception.js
@@ -1,14 +1,19 @@
-function ValidationException(message) {
-    this.message = message;
-    // Use V8's native method if available, otherwise fallback
-    if ("captureStackTrace" in Error)
-        Error.captureStackTrace(this, ValidationException);
-    else
-        this.stack = (new Error()).stack;
+class ValidationException extends Error {
+
+    constructor(message) {
+        super(message)
+        this.name = "ValidationException"
+
+        // Use V8's native method if available, otherwise fallback
+        if ("captureStackTrace" in Error)
+            Error.captureStackTrace(this, ValidationException);
+        else
+            this.stack = (new Error()).stack;
+    }
+
 }
 
-ValidationException.prototype = Object.create(Error.prototype);
-ValidationException.prototype.name = "ValidationException";
-ValidationException.prototype.constructor = ValidationException;
+
+
 
 module.exports = ValidationException;
