1e46d988376b17b3116c6e264c709c79cf1e7dd2
Patrick Toner <pat.toner2@gmail.com>
2019-02-03 20:18:57 -0500

    ipfs config

--- truffle.js
+++ truffle.js
@@ -2,6 +2,10 @@
 //   // See <http://truffleframework.com/docs/advanced/configuration>
 //   // to customize your Truffle configuration!
 // };
+require('dotenv').config()
+
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
 
 module.exports = {
   networks: {
@@ -10,6 +14,12 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
       gas: 6109322
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(
+          process.env.MNEMONIC,
+          process.env.ROPSTEN_URL),
+      network_id: 3
     }
   }
-};
+}
